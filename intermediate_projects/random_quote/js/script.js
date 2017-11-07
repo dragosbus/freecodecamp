a1f22324ed2feed5eb2681c9bdc433b2df3fc824
dragosbus <dragosbusuioc94@gmail.com>
2017-11-07 14:59:34 +0100

    Style adjustments

--- intermediate_projects/random_quote/js/script.js
+++ intermediate_projects/random_quote/js/script.js
@@ -31,7 +31,9 @@ function getQuote() {
       $(".quote-tumblr").attr("href", 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' + author + '&content=' + quote + '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
     },
     error: function () {
-
+      changeColor();
+      QUOTE.css('color','#df4d4d');
+      QUOTE.text("There is an error!Please try later!");
     }
   });
 }
