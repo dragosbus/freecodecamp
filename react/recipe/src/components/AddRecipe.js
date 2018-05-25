981b3431e9652ab24df3a25f352359454db20bf7
dragosbus <dragosbusuioc94@gmail.com>
2018-05-25 14:41:34 +0200

    created modal where will be added new recipe

---
+++ react/recipe/src/components/AddRecipe.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+
+export class AddRecipeModal extends Component {
+    render() {
+        return (
+            <div className="modal">
+                <div className="modal-header">
+                    <h3>Add Recipe</h3>
+                    <button className="close-modal">x</button>
+                </div>
+                <div className="body-modal">
+                    <div>
+                        <label htmlFor="recipe-name">Recipe</label>
+                        <input type="text" id="recipe-name" placeholder="Recipe Name" />
+                    </div>
+                    <div>
+                        <label htmlFor="ingredients">Ingredients</label>
+                        <textarea id="Ingredients" placeholder="Enter Ingredients, Separated by Commas"></textarea>
+                    </div>
+                </div>
+                <div className="footer-modal">
+                    <button className="btn">Add Recipe</button>
+                    <button className="btn">Close</button>
+                </div>
+            </div>
+        );
+    }
+} 
+
---
+++ react/recipe/src/components/Recipes.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export class Recipes extends Component {
+    render() {
+        return (
+            <ul className="recipes">
+            
+            </ul>
+        );
+    }
+}
